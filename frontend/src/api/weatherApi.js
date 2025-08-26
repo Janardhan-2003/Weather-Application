4a9361eb2989c46d57aa8545b23adb5923d2f7e4
Janardhan-2003 <kokatam71714@gmail.com>
2025-08-27 00:08:11 +0530

    Debugging done

--- frontend/src/api/weatherApi.js
+++ frontend/src/api/weatherApi.js
@@ -3,37 +3,37 @@ import axios from "axios";
 const API_BASE = "https://weather-application-hxb0.onrender.com";
 
 export const getWeather = async (city) => {
-  const res = await axios.get(`${API_BASE}/weather/current/${city}`);
+  const res = await axios.get(`${API_BASE}/api/weather/current/${city}`);
   return res.data;
 };
 
 export const getForecast = async (city) => {
-  const res = await axios.get(`${API_BASE}/weather/forecast/${city}`);
+  const res = await axios.get(`${API_BASE}/api/weather/forecast/${city}`);
   return res.data;
 };
 
 export const getFavourites = async (uid) => {
-  const res = await axios.get(`${API_BASE}/favourites/${uid}`);
+  const res = await axios.get(`${API_BASE}/api/favourites/${uid}`);
   return res.data;
 };
 
 export const addFavourite = async (uid, cityName) => {
-  const res = await axios.post(`${API_BASE}/favourites`, { uid, cityName });
+  const res = await axios.post(`${API_BASE}/api/favourites`, { uid, cityName });
   return res.data;
 };
 
 export const deleteFavourite = async (uid, cityName) => {
-  const res = await axios.delete(`${API_BASE}/favourites/${uid}/${cityName}`);
+  const res = await axios.delete(`${API_BASE}/api/favourites/${uid}/${cityName}`);
   return res.data;
 };
 
 export const getPreferences = async (uid) => {
-  const res = await axios.get(`${API_BASE}/preferences/${uid}`);
+  const res = await axios.get(`${API_BASE}/api/preferences/${uid}`);
   return res.data;
 };
 
 export const savePreferences = async (uid, unit, location) => {
-  const res = await axios.post(`${API_BASE}/preferences`, {
+  const res = await axios.post(`${API_BASE}/api/preferences`, {
     uid,
     unit,
     location,
